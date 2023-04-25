4ee67433503502743ed27ce0690616c954a6ed96
BryanSabio <gomezperezb8@gmail.com>
2023-04-25 10:19:49 -0600

    https

--- lib/config.dart
+++ lib/config.dart
@@ -1,7 +1,7 @@
 class Config {
   static const String appName = "Welcome";
   //static const String apiURL = '192.168.1.40';
-  static const String apiURL = 'https://modular3-production.up.railway.app';
+  static const String apiURL = 'modular3-production.up.railway.app';
   static const productosAPI = "api/productopequeno";
   static const productosmAPI = "api/productomediano";
   static const productosgAPI = "api/productogrande";
